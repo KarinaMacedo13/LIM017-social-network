bcd2fbffcc9b2249dde78313585fb0fa76e2b63d
Karina <ruth.macedo@ucsp.edu.pe>
2022-03-21 12:52:11 -0600

    login, signup

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB1Voc6sDbKr4Py0D90sREgnsbOS8V9QWg',
+  authDomain: 'social-media-befit.firebaseapp.com',
+  projectId: 'social-media-befit',
+  storageBucket: 'social-media-befit.appspot.com',
+  messagingSenderId: '1024930608861',
+  appId: '1:1024930608861:web:fc5e31e75040e33e788d34',
+  measurementId: 'G-4FPZYCWMCE',
+};
+// Initialize Firebase
+export const firebaseBefit = firebase.initializeApp(firebaseConfig);
