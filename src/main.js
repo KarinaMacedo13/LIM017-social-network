fc50dea97227fdb7e4b751c3466c326261c897ed
LissetteMH <lissette.machaca@gmail.com>
2022-04-04 19:01:57 -0500

    creating controller folder, and registerDOM file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-cycle */
 import { home } from './templates/home.js';
 import { register } from './templates/register.js';
-import {
-  registerFirebase, registerGoogle, loginFirebase, logInGoogle, cerrar,
-} from './lib/firebase.js';
+import { loginFirebase, logInGoogle, cerrar } from './lib/firebase.js';
 import { login } from './templates/login.js';
+import { showRegister } from './controlles/registerDOM.js';
 
 const rootDiv = document.getElementById('root');
 const routes = {
@@ -16,7 +15,7 @@ function loadPage() {
   const component = window.location.pathname;
   rootDiv.innerHTML = routes[component]();
 }
-const onNavigate = (pathname) => {
+export const onNavigate = (pathname) => {
   window.history.pushState(
     {},
     pathname,
@@ -41,49 +40,49 @@ export function registerToHome() {
   showHome();
 }
 
-function showRegister() {
-  // btn LogIn, from Register to Log in
-  const logInHere = document.getElementById('logInHere');
-  const name = document.querySelector('#name');
-  const lastName = document.querySelector('#lastName');
-  name.addEventListener('keyup', (e) => {
-    const valorName = e.target.value;
-    console.log(valorName);
-    name.value = valorName.replace(/\d/g, '');// no permite números y letras mayúsculas
-  });
-  lastName.addEventListener('keyup', (e) => {
-    const valorLastName = e.target.value;
-    console.log(valorLastName);
-    lastName.value = valorLastName.replace(/\d/g, '');// no permite números y letras mayúsculas
-  });
-  logInHere.addEventListener('click', () => {
-    onNavigate('/');
-    // eslint-disable-next-line no-use-before-define
-    showLogin();
-  });
-  // SignUp button -> LogIn
-  const btnRegister = document.getElementById('btnRegister');
-  btnRegister.addEventListener('click', () => {
-    const email = document.querySelector('#emailSignUp').value;
-    console.log(email);
-    const password = document.querySelector('#passwordSignUp').value;
-    // validar espacios
-    if ((name.value === '') || (lastName.value === '') || (email === '') || (password === '')) {
-      console.log('Campos vacios');
-      // e.preventDefault();
-      const messageSignUpError = document.querySelector('.messageSignUpError');
-      messageSignUpError.innerHTML = 'Fill in the missing field';
-    } else {
-      registerFirebase(email, password);
-    }
-  });
-  // Button Google
-  const btnSignUpGoogle = document.getElementById('btnSignUpGoogle');
-  btnSignUpGoogle.addEventListener('click', () => {
-    registerGoogle();
-    // onNavigate('/');
-  });
-}
+// function showRegister() {
+//   // btn LogIn, from Register to Log in
+//   const logInHere = document.getElementById('logInHere');
+//   const name = document.querySelector('#name');
+//   const lastName = document.querySelector('#lastName');
+//   name.addEventListener('keyup', (e) => {
+//     const valorName = e.target.value;
+//     console.log(valorName);
+//     name.value = valorName.replace(/\d/g, '');// no permite números y letras mayúsculas
+//   });
+//   lastName.addEventListener('keyup', (e) => {
+//     const valorLastName = e.target.value;
+//     console.log(valorLastName);
+//     lastName.value = valorLastName.replace(/\d/g, '');// no permite números y letras mayúsculas
+//   });
+//   logInHere.addEventListener('click', () => {
+//     onNavigate('/');
+//     // eslint-disable-next-line no-use-before-define
+//     showLogin();
+//   });
+//   // SignUp button -> LogIn
+//   const btnRegister = document.getElementById('btnRegister');
+//   btnRegister.addEventListener('click', () => {
+//     const email = document.querySelector('#emailSignUp').value;
+//     console.log(email);
+//     const password = document.querySelector('#passwordSignUp').value;
+//     // validar espacios
+//     if ((name.value === '') || (lastName.value === '') || (email === '') || (password === '')) {
+//       console.log('Campos vacios');
+//       // e.preventDefault();
+//       const messageSignUpError = document.querySelector('.messageSignUpError');
+//       messageSignUpError.innerHTML = 'Fill in the missing field';
+//     } else {
+//       registerFirebase(email, password);
+//     }
+//   });
+//   // Button Google
+//   const btnSignUpGoogle = document.getElementById('btnSignUpGoogle');
+//   btnSignUpGoogle.addEventListener('click', () => {
+//     registerGoogle();
+//     // onNavigate('/');
+//   });
+// }
 
 function showLogin() {
   // SignHere -> register
